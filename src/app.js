57f5b5ee5576cca08a3c96f6a02041395de43f2a
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-12-03 11:27:24 -0700

    Testing 4

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require('express')
 
 const app = express()
 
-//const {accounts, users, writeJSON} = require('data.js')
+const {accounts, users, writeJSON} = require('./data.js')
 
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
