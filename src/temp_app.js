413ee5bc0593e52053984164600348a5f5e10f46
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-12-03 11:21:38 -0700

    Trouble Shooting module 4

--- src/temp_app.js
+++ src/temp_app.js
@@ -5,7 +5,7 @@ const express = require('express')
 
 const app = express()
 
-//const {accounts, users, writeJSON} = require('data.js')
+const {accounts, users, writeJSON} = require('data.js')
 
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
@@ -45,6 +45,6 @@ app.post('/transfer', (req, res) => {
     res.render('transfer', {message: "Transfer Completed"})
 })
 
-app.listen( 3000, () => console.log("PS Project Running on port 3000!"))
+app.listen(3000);
 
 //Module 2 Code
