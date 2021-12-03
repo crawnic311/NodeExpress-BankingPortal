afc7291dc2ed7fa4c6be98c0041ee98538b5281a
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-12-03 11:16:08 -0700

    Module 4 attempt

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const express = require('express')
 
 const app = express()
 
+const {accounts, users, writeJSON} = require('data.js')
+
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
 
---
+++ src/data.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+
+const accounts = JSON.parse(accountData);
+fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+
+const users = JSON.parse(userData)
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8')
+
+writeJSON => {
+    const accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+}
+
+module.exports(accounts, users, writeJSON)
